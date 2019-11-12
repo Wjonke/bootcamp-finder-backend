970210f0a47008745c692d4c692b97eb2a75fa9b
Wes Jonke <wesjonke@gmail.com>
2019-11-12 14:46:20 -0500

    added mongoose and colors dep. added colors to terminal logs for Server and DB

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,9 +1,11 @@
+const colors = require('colors');
+
 /////custom built logger middleware, not used currently////////
 // @desc     Logs request-type and url to console
 const logger = (req, res, next) => {
   console.log(
     `"${req.method}" request made to -> ${req.protocol}://${req.get('host')}${
-      req.originalUrl
+      req.originalUrl.purple.underline.bold
     }`
   );
   next();
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
+
+const server = express();
+
+////setting up environmental variables////
+dotenv.config({ path: './config/config.env' });
+
+const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'development') {
+  server.use(morgan('dev'));
+}
+
+connectDB();
+////setting up environmental variables////
 
 ////import middleware files////
 const logger = require('./middleware/logger');
@@ -10,26 +25,21 @@ const logger = require('./middleware/logger');
 const bootcamps = require('./routes/bootcamps');
 //////import Route files//////
 
-const server = express();
-
-////setting up environmental variables////
-dotenv.config({ path: './config/config.env' });
-const PORT = process.env.PORT || 5000;
-////setting up environmental variables////
-
-///////Development env logging middleware////////
-if (process.env.NODE_ENV === 'development') {
-  server.use(morgan('dev'));
-}
-///////only runs in Dev mode////////
-
 //Mount routers
 //this is where I set a base url specified to the second param!
 server.use('/api/v1/bootcamps', bootcamps);
 
-server.listen(
+const app = server.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
+
+///handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  //close server and exit process
+  app.close(() => process.exit(1));
+});
